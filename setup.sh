28d5a0adeefc4d7a773022cefd2027af7cc605e5
vasylcode <vasylcode@gmail.com>
2022-10-08 20:31:07 +0200

    en lang

--- setup.sh
+++ setup.sh
@@ -68,20 +68,40 @@ sleep 10
 sudo systemctl restart subspaced-farmer
 
 echo -e "\n\e[93mSubspace Gemini II\e[0m"
-echo -e '\n\e[94mСтатус ноди\e[0m\n' && sleep 1
-if [[ `service subspaced status | grep active` =~ "running" ]]; then
-  echo -e "Ваша Subspace нода \e[92mвстановлена та працює\e[0m!"
-  echo -e "Перевірити статус Вашої ноди можна командою \e[92mservice subspaced status\e[0m"
-  echo -e "Натисність \e[92mQ\e[0m щоб вийти з статус меню"
+if [ "$language" = "uk" ]; then
+  echo -e '\n\e[94mСтатус ноди\e[0m\n' && sleep 1
+  if [[ `service subspaced status | grep active` =~ "running" ]]; then
+    echo -e "Ваша Subspace нода \e[92mвстановлена та працює\e[0m!"
+    echo -e "Перевірити статус Вашої ноди можна командою \e[92mservice subspaced status\e[0m"
+    echo -e "Натисність \e[92mQ\e[0m щоб вийти з статус меню"
+  else
+    echo -e "Ваша Subspace нода \e[91mбула встановлена неправильно\e[39m, виконайте перевстановлення."
+  fi
+  sleep 2
+  echo -e '\n\e[94mFarmer статус\e[0m\n' && sleep 1
+  if [[ `service subspaced-farmer status | grep active` =~ "running" ]]; then
+    echo -e "Ваш Subspace farmer \e[92mвстановлений та працює\e[0m!"
+    echo -e "Перевірити статус Вашого farmer можна командою \e[92mservice subspaced-farmer status\e[0m"
+    echo -e "Натисність \e[92mQ\e[0m щоб вийти з статус меню"
+  else
+    echo -e "Ваш Subspace farmer \e[91mбув встановлений неправильно\e[39m, виконайте перевстановлення."
+  fi
 else
-  echo -e "Ваша Subspace нода \e[91mбула встановлена неправильно\e[39m, виконайте перевстановлення."
-fi
-sleep 2
-echo -e '\n\e[94mFarmer статус\e[0m\n' && sleep 1
-if [[ `service subspaced-farmer status | grep active` =~ "running" ]]; then
-  echo -e "Ваш Subspace farmer \e[92mвстановлений та працює\e[0m!"
-  echo -e "Перевірити статус Вашого farmer можна командою \e[92mservice subspaced-farmer status\e[0m"
-  echo -e "Натисність \e[92mQ\e[0m щоб вийти з статус меню"
-else
-  echo -e "Ваш Subspace farmer \e[91mбув встановлений неправильно\e[39m, виконайте перевстановлення."
+  echo -e '\n\e[94mNode Status\e[0m\n' && sleep 1
+  if [[ `service subspaced status | grep active` =~ "running" ]]; then
+    echo -e "Your Subspace node \e[92msuccessfully installed and running\e[0m!"
+    echo -e "Check your node status: \e[92mservice subspaced status\e[0m"
+    echo -e "Press \e[92mQ\e[0m to exit menu"
+  else
+    echo -e "Your Subspace Node \e[91mwas not installed correctly\e[39m, please reinstall."
+  fi
+  sleep 2
+  echo -e '\n\e[94mFarmer status\e[0m\n' && sleep 1
+  if [[ `service subspaced-farmer status | grep active` =~ "running" ]]; then
+    echo -e "Your Subspace farmer \e[92msuccessfully installed and running\e[0m!"
+    echo -e "Check your farmer status: \e[92mservice subspaced-farmer status\e[0m"
+    echo -e "Press \e[92mQ\e[0m to exit menu"
+  else
+    echo -e "Your Subspace Farmer \e[91mwas not installed correctly\e[39m, please reinstall."
+  fi
 fi

9c6a9eed8d9e279ff121e1d9402baf22ac75fba9
Influenzo <52459025+vasylcode@users.noreply.github.com>
2023-09-22 12:18:24 -0600

    v0.6.9-alpha

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ fi
 
 sudo apt update && sudo apt install ocl-icd-opencl-dev libopencl-clang-dev libgomp1 -y
 cd $HOME
-wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.8-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.8-alpha
+wget -O pulsar https://github.com/subspace/pulsar/releases/download/v0.6.9-alpha/pulsar-ubuntu-x86_64-skylake-v0.6.9-alpha
 sudo chmod +x pulsar
 sudo mv pulsar /usr/local/bin/
 sudo rm -rf $HOME/.config/pulsar
